c2b1a649da8528c87669e81eb1b62aca25abb1e0
huynhgiahuy0409 <18130094@st.hcmuaf.edu.vn>
2021-09-22 07:46:11 +0700

    .

--- src/main/java/com/laptrinhjavaweb/constance/SystemContance.java
+++ src/main/java/com/laptrinhjavaweb/constance/SystemContance.java
@@ -1,7 +1,7 @@
 package com.laptrinhjavaweb.constance;
 
 public class SystemContance {
-	public static final long EXPIRATION_TIME = 20000;
+	public static final long EXPIRATION_TIME = 30000;
 	public static final String SECREC = "huynhgiahuy";
 	public static final String TOKEN_PREFIX = "Bearer";
 	public static final String AUTHORIZATION_KEY_IN_HEADER = "Authorization";
